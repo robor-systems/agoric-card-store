f0c841c0e1c5f18c76eae74e8391c900a5c2d10a
haseebasim <haseebzaidi123@gmail.com>
2021-12-21 17:18:00 +0500

    chore: agoric init my-card-store
    Imported from git://github.com/Agoric/dapp-card-store.git

---
+++ ui/src/conf/defaults.js
@@ -0,0 +1,18 @@
+// GENERATED FROM /Users/katesills/code/dapp-card-store/api/deploy.js
+export default {
+  "INSTANCE_BOARD_ID": "794977354",
+  "INSTALLATION_BOARD_ID": "379944443",
+  "SELL_ITEMS_INSTALLATION_BOARD_ID": "1530295560",
+  "INVITE_BRAND_BOARD_ID": "1456154132",
+  "brandBoardIds": {
+    "Card": "529406044",
+    "Money": "1576549616"
+  },
+  "issuerBoardIds": {
+    "Card": "715805464",
+    "Money": "1667979430"
+  },
+  "BRIDGE_URL": "http://127.0.0.1:8000",
+  "API_URL": "http://127.0.0.1:8000",
+  "CONTRACT_NAME": "cardStore"
+};
