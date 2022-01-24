28bff6b17d39e1f977f06fed5c981f316bd08b61
haseebasim <haseebzaidi123@gmail.com>
2022-01-24 12:11:07 +0500

    public-facet-issue

--- ui/src/conf/defaults.js
+++ ui/src/conf/defaults.js
@@ -1,21 +1,21 @@
 // GENERATED FROM /home/haseeb/agoric/dapps/my-card-store/api/deploy.js
 export default {
-  "INSTANCE_BOARD_ID": "984638224",
-  "INSTALLATION_BOARD_ID": "474502210",
-  "AUCTION_ITEMS_INSTALLATION_BOARD_ID": "688500812",
+  "INSTANCE_BOARD_ID": "93006813",
+  "INSTALLATION_BOARD_ID": "1572641147",
+  "AUCTION_ITEMS_INSTALLATION_BOARD_ID": "192932059",
   "INVITE_BRAND_BOARD_ID": "1265655452",
   "BRIDGE_URL": "agoric-lookup:https://local.agoric.com?append=/bridge",
   "brandBoardIds": {
-    "Card": "487997721",
+    "Card": "614751240",
     "Money": "1532665031"
   },
   "issuerBoardIds": {
-    "Card": "596594455",
+    "Card": "1178513834",
     "Money": "1576549616"
   },
   "minBidPerCard": 1000000,
   "API_URL": "http://127.0.0.1:8000",
   "CONTRACT_NAME": "cardStore",
-  "ATOMICSWAP_CONTRACT_INSTANCE_BOARD_ID": "1026677745",
-  "ATOMICSWAP_CONTRACT_PUBLIC_FACET_BOARD_ID": "931423413"
+  "ATOMICSWAP_CONTRACT_INSTANCE_BOARD_ID": "254080728",
+  "ATOMICSWAP_CONTRACT_PUBLIC_FACET_BOARD_ID": "1493185302"
 };
